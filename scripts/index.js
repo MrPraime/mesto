9127211f35c49b4dda0b5a19a3e7405e694dd7ef
MrPraime <69200777+MrPraime@users.noreply.github.com>
2021-04-25 19:47:04 +0300

    Финальные правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
-let Popup = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close-button');
 let editProfileButton = document.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.popup__form'); 
@@ -7,18 +7,20 @@ let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_about');
 let profileName = document.querySelector('.popup__form').elements.name;
 let about = document.querySelector('.popup__form').elements.about;
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
 
 /*Функция открытия попапа со значениями передаваемыми из profile__title  */
 function openPopup() {
-	Popup.classList.add('popup_is-opened');
-	profileName.value = document.querySelector('.profile__title').textContent;
-	about.value = document.querySelector('.profile__subtitle').textContent;
+	popup.classList.add('popup_is-opened');
+	profileName.value = title.textContent;
+	about.value = subtitle.textContent;
 }
 
 /*Функция закрытия*/
 
 function closePopup() {
-	Popup.classList.remove('popup_is-opened');
+	popup.classList.remove('popup_is-opened');
 }
 
 /*Функция внесения новых значений из инпутов*/
@@ -27,9 +29,6 @@ function formSubmitHandler(evt) {
 	
 	let name = nameInput.value
 	let job = jobInput.value;
-	
-	let title = document.querySelector('.profile__title');
-	let subtitle = document.querySelector('.profile__subtitle');
 	
 	title.textContent = name;
 	subtitle.textContent = job;
